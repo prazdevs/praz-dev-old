acda1a61d46ecd3ddb5aad7ece1a5cc38574480a
PraZ <sachabouillez@gmail.com>
2020-05-25 15:22:32 +0200

    add twitter link to footer

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -19,6 +19,15 @@ const Footer = () => (
         <i className="icon-github" />
       </a>
       <a
+        className="link link-twitter"
+        href="https://twitter.com/PrazDevs"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="Twitter Profile"
+      >
+        <i className="icon-twitter" />
+      </a>
+      <a
         className="link link-linkedin"
         href="https://www.linkedin.com/in/sacha-bouillez-8aa657165/"
         target="_blank"
