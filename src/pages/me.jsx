0f7d79393c7ca304a3f810c2fac84de051175111
PraZ <sachabouillez@gmail.com>
2020-07-19 22:02:50 +0200

    externalize content out of components properly

--- src/pages/me.jsx
+++ src/pages/me.jsx
@@ -7,69 +7,14 @@ import Layout from '../layout/layout';
 
 export default ({ data }) => {
   const about = data.about.html;
-
-  const stack = [
-    {
-      name: 'Back-end',
-      techs: [
-        { name: 'NodeJS', link: 'https://nodejs.org/en/' },
-        { name: 'Express', link: 'https://expressjs.com/' },
-        { name: 'NestJS', link: 'https://nestjs.com/' },
-        { name: 'MongoDB', link: 'https://www.mongodb.com/' },
-        { name: 'Firebase', link: 'https://firebase.google.com/' },
-      ],
-    },
-    {
-      name: 'Front-end',
-      techs: [
-        { name: 'React', link: 'https://reactjs.org/' },
-        { name: 'Redux', link: 'https://redux.js.org/' },
-        { name: 'Gatsby', link: 'https://www.gatsbyjs.org/' },
-        { name: 'Vue', link: 'https://vuejs.org/' },
-        { name: 'Svelte', link: 'https://svelte.dev' },
-        { name: 'Sass', link: 'https://sass-lang.com/' },
-      ],
-    },
-    {
-      name: 'Other technologies',
-      techs: [
-        { name: 'GraphQL', link: 'https://graphql.org/' },
-        { name: 'Apollo', link: 'https://www.apollographql.com/' },
-        { name: 'Jest', link: 'https://jestjs.io/' },
-        { name: 'Cypress', link: 'https://cypress.io/' },
-        { name: 'ESLint', link: 'https://eslint.org/' },
-        { name: 'Yarn', link: 'https://yarnpkg.com/' },
-      ],
-    },
-  ];
-
-  const timeline = [
-    {
-      text: 'Started writing my dev blog and sharing my love of Javascript',
-      date: 'Dec. 2020',
-      tag: 'project',
-    },
-    {
-      text: 'Started working at Neoledge as Software Engineer (C#/VueJs)',
-      date: 'April 2019',
-      tag: 'work',
-    },
-    {
-      text: "Obtained a bachelor's degree in web development",
-      date: 'April 2019',
-      tag: 'education',
-    },
-    {
-      text: "Obtained a bachelor's degree in general Computer Science",
-      date: 'June 2017',
-      tag: 'education',
-    },
-  ];
+  const stack = data.about.frontmatter.stack;
+  const timeline = data.about.frontmatter.timeline;
+  const text = data.about.frontmatter.text;
 
   return (
     <Layout>
       <SEO title="About Me" />
-      <About stack={stack} about={about} timeline={timeline} />
+      <About stack={stack} about={about} timeline={timeline} text={text} />
     </Layout>
   );
 };
@@ -78,6 +23,24 @@ export const query = graphql`
   query {
     about: markdownRemark(frontmatter: { category: { eq: "about-me" } }) {
       html
+      frontmatter {
+        text
+        stack {
+          name
+          techs {
+            name
+            link
+          }
+        }
+        timeline {
+          text
+          events {
+            text
+            tag
+            date
+          }
+        }
+      }
     }
   }
 `;
