378ef58a3f9bb7ff6e88b1488489a399284a2f02
PraZ <sachabouillez@gmail.com>
2020-10-13 20:56:06 +0200

    make filter tags accessible

--- src/components/tag-filter/tag-filter.component.jsx
+++ src/components/tag-filter/tag-filter.component.jsx
@@ -3,18 +3,28 @@ import React from 'react';
 import Tag from '../tag/tag.component';
 
 const TagFilter = ({ tags, handleTagClick }) => {
+  const handleTagKeyPress = (e, tag) => {
+    e.preventDefault();
+    if (e.key === ' ') {
+      handleTagClick(tag);
+    }
+  };
+
   return (
     <div className="tag-filter-container">
       <span>Only looking for specific content ?</span>
       <div className="tag-filter">
-        {tags.map(tag => (
-          <div
+        {tags.map((tag) => (
+          <a
+            href="#"
+            role="button"
+            className="tag-button"
             key={tag.fieldValue}
+            onKeyPress={(e) => handleTagKeyPress(e, tag.fieldValue)}
             onClick={() => handleTagClick(tag.fieldValue)}
-            role="presentation"
           >
             <Tag name={tag.fieldValue} clickable selected={tag.selected} />
-          </div>
+          </a>
         ))}
       </div>
     </div>
